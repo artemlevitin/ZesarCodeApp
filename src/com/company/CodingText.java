bcf903917df2383dc27e9b03e382242570fe410a
artemlevitin <levitin@live.com>
2018-07-23 16:04:17 +0300

    completed for key no more 4

--- src/com/company/CodingText.java
+++ src/com/company/CodingText.java
@@ -6,16 +6,14 @@ import java.nio.file.Paths;
 
 
 public  class CodingText implements Constants {
-    //  static public execute(String fileputh, int key){
-    //private byte bufTxt[];
 
 
     static public void codding(String filePath, int key) {
         try {
             byte[] buf = Files.readAllBytes(Paths.get(filePath));
             for (int i = 0; i < buf.length; ++i) {
-               if(CheckLetter.result(buf[i])!= NOT_LETTER)
-                   buf[i] += key;
+                if (CheckLetter.result(buf[i]) != NOT_LETTER)
+                    buf[i] += key;
             }
 
             int p = filePath.lastIndexOf(".");
@@ -33,11 +31,11 @@ public  class CodingText implements Constants {
             byte[] buf = Files.readAllBytes(Paths.get(filePath));
 
             for (int i = 0; i < buf.length; ++i) {
-                if (CheckLetter.result( buf[i]-key)!= NOT_LETTER)
-                buf[i] -= key;
+                if (CheckLetter.result(buf[i] - key) != NOT_LETTER)
+                    buf[i] -= key;
             }
 
-            return new BufferedReader( new InputStreamReader(new ByteArrayInputStream(buf)));
+            return new BufferedReader(new InputStreamReader(new ByteArrayInputStream(buf)));
 
            /* int p = filePath.lastIndexOf(".");
             String newFilePath = filePath.substring(0, p) + "_Code" + filePath.substring(p);
@@ -48,18 +46,17 @@ public  class CodingText implements Constants {
         }
     }
 
-    static public ByteArrayInputStream  encodding(ByteArrayInputStream bais, int key) {
+    static public ByteArrayInputStream encodding(ByteArrayInputStream bais, int key) {
         try {
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
             int b;
-            while ((  b = bais.read())!= -1)
-            {
+            while ((b = bais.read()) != -1) {
                 b -= key;
                 baos.write(b);
-          }
-           byte [] byteAr = baos.toByteArray();
+            }
+            byte[] byteAr = baos.toByteArray();
 
-          return new ByteArrayInputStream(byteAr);
+            return new ByteArrayInputStream(byteAr);
 
         } catch (Exception exc) {
             System.out.print(exc.toString());
@@ -67,21 +64,49 @@ public  class CodingText implements Constants {
         }
     }
 
-    static public byte[]  encodding(byte [] inpByte , int key) {
+    static private byte[] encodding(byte[] inpByte, int key) {
         try {
-           byte [] outBytes = new byte[inpByte.length];
-           System.arraycopy(inpByte,0,outBytes,0,inpByte.length);
+            byte[] outBytes = new byte[inpByte.length];
+            System.arraycopy(inpByte, 0, outBytes, 0, inpByte.length);
 
-            for(int i=0; i<outBytes.length; ++i) {
-                if(CheckLetter.result(outBytes[i])!=NOT_LETTER)
-                outBytes[i] -= key;
-             }
+            for (int i = 0; i < outBytes.length; ++i) {
+                if (CheckLetter.result(outBytes[i] - key) != NOT_LETTER)
+                    outBytes[i] -= key;
+            }
 
             return outBytes;
 
         } catch (Exception exc) {
             System.out.print(exc.toString());
             return null;
+        }
+    }
+
+    static public void encoddingFile(String filePath) {
+        try {
+            byte[] buf1 = Files.readAllBytes(Paths.get(filePath));
+            byte[] buf2;
+
+
+            double[] freqBuf1 = new FreqLetters(buf1).get();
+            double[] freqBuf2;
+
+
+            for (int key = 1; key < 12; ++key) {
+                buf2 = encodding(buf1, key);
+                freqBuf2 = new FreqLetters(buf2).get();
+                if (Comparer.getPrecission(freqBuf1) > Comparer.getPrecission(freqBuf2)) {
+                    buf1 = buf2;
+                    freqBuf1 = freqBuf2;
+                }
+            }
+
+            int p = filePath.lastIndexOf(".");
+            String newFilePath = filePath.substring(0, p) + "_Encode" + filePath.substring(p);
+            Files.write(Paths.get(newFilePath), buf1);
+
+        } catch (Exception exc) {
+            System.out.print(exc.toString());
         }
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,32 +8,14 @@ import java.nio.file.Paths;
 public class Main {
 
     public static void main(String[] args) {
-        //  CodingText.codding("Text.txt",3);
+        CodingText.codding("Text.txt",4);
 
-        String filePath = "Text_Code.txt";
-        byte[] buf = readFile(filePath);
-        byte[] buf2;
-        //CodingText.encodding(ReadFile(filePath),);
-        //System.out.println( Comparer.getPrecission( new FreqLetters(buf).get() ) );
-        double[] freqBuf1 = new FreqLetters(buf).get();
-        double[] freqBuf2;
-       // double[] freqBuf2 = new FreqLetters(buf2).get();
-        for(int key=1; key < 12; ++key) {
-            buf2 = CodingText.encodding(buf,key);
-            freqBuf2 = new FreqLetters(buf2).get();
-            if (Comparer.getPrecission(freqBuf1) > Comparer.getPrecission(freqBuf2)) {
-                buf = buf2;
-                freqBuf1 = freqBuf2;
-            }
-        }
-       // buf = CodingText.encodding(buf,3);
+        CodingText.encoddingFile("Text_Code.txt");
 
-        writeFile(buf,filePath);
-       // System.out.println( Comparer.getPrecission( new FreqLetters(buf2).get() ) );
 
-       // System.out.println(Comparer.getPrecission((fileFreqLet(filePath))));
 
-        //  CodingText.encodding("Text_Code.txt",3);
+
+
 
     }
 
@@ -57,6 +39,7 @@ public class Main {
             System.out.println(ex.getMessage());
         }
     }
+
 
     private static BufferedReader ReadFile(String filePath) {
         try(BufferedReader bufRd = new BufferedReader(new FileReader(filePath))) {
